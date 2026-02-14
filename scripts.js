b6eb1084fa65d2263330223326b0a09c688594bd
francisb <byaruhangafrancis23@gmail.com>
2026-02-14 15:07:09 +0300

    Create getComputerChoice function to retrieve computer choice

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+    // Generate a random number between 0 & 2
+    const randomNumber = Math.random();
+
+    // Convert to a choice between 1 & 2 and map the choice to Rock, Paper or Scissors
+    if (randomNumber < 0.33) {
+        return "Rock";
+    } else if (randomNumber < 0.66) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice);
